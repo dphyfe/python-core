b70d5a76a7e632d8d8710edbf6f33696162f1a81
dphyfe <davidtphyfe@gmail.com>
2025-01-05 13:35:00 -0500

    Day 5: Python basics - Loops (part 3/3)

--- day05_loops.py
+++ day05_loops.py
@@ -104,5 +104,56 @@ print("My total sum:", total)
 # Summing more numbers for my practice
 my_values = [10, 20, 30]
 my_sum = 0
+for value in my_values:
+    my_sum = my_sum + value
+print("My sum of values:", my_sum)
 
-# Progress: part 2/3
+# Building a list of doubled numbers
+doubled = []
+for number in numbers:
+    doubled.append(number * 2)
+print("My doubled numbers:", doubled)
+
+# Enumerate in my loop (8)
+print("My indexed mountains:")
+for index, mountain in enumerate(mountains):
+    print("Position", index, "has", mountain)
+
+# Enumerate with my foods
+print("My indexed foods:")
+for i, food in enumerate(foods):
+    print(i, ":", food)
+
+# Enumerate starting from 1 for my list
+print("My mountains starting count at 1:")
+for position, mountain in enumerate(mountains, start=1):
+    print(position, mountain)
+
+# Nested loop for my combinations (9)
+print("My mountain + food pairs:")
+for mountain in mountains:
+    for food in foods:
+        print(mountain, "with", food)
+
+# Another nested loop example I'm practicing
+colors = ["blue", "green"]
+sizes = ["small", "large"]
+print("My color and size combos:")
+for color in colors:
+    for size in sizes:
+        print(color, size)
+
+# Loop to collect only long names
+long_mountains = []
+for mountain in mountains:
+    if len(mountain) > 5:
+        long_mountains.append(mountain)
+print("My long mountain names:", long_mountains)
+
+# Loop to check if any food starts with 't'
+print("My foods starting with 't':")
+for food in foods:
+    if food.startswith("t"):
+        print(food)
+
+# Progress: part 3/3
