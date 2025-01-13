ddba5d4e8ba33af149e0930fc1099eac1dfd6ae5
dphyfe <davidtphyfe@gmail.com>
2025-01-13 12:19:00 -0500

    Day 13: Python basics - Tuples Sets (part 2/3)

--- day13_tuples_sets.py
+++ day13_tuples_sets.py
@@ -34,3 +34,40 @@ for mountain in my_mountains:
 
 # My sets - unique items only! (6)
 my_set = {1, 2, 3, 4, 5}
+print("My set:", my_set)
+
+my_colors_set = {"blue", "green", "red"}
+print("My colors set:", my_colors_set)
+
+# My set from list (removes duplicates) (7)
+my_with_dupes = [1, 2, 2, 3, 3, 3, 4]
+my_unique_set = set(my_with_dupes)
+print("My unique set:", my_unique_set)
+
+# Another duplicate removal
+my_words = ["dog", "cat", "dog", "bird", "cat"]
+my_unique_words = set(my_words)
+print("My unique words:", my_unique_words)
+
+# My set operations - adding (8)
+my_activities = {"hiking", "coding"}
+my_activities.add("reading")
+print("My activities after add:", my_activities)
+
+my_activities.add("hiking")  # won't add duplicate
+print("My activities after adding duplicate:", my_activities)
+
+# My set removing items (9)
+my_numbers_set = {1, 2, 3, 4, 5}
+my_numbers_set.remove(3)
+print("My set after remove:", my_numbers_set)
+
+my_numbers_set.discard(10)  # won't error if not found
+print("My set after discard:", my_numbers_set)
+
+# My set union (10)
+my_set1 = {1, 2, 3}
+my_set2 = {3, 4, 5}
+my_union = my_set1 | my_set2
+
+# Progress: part 2/3
