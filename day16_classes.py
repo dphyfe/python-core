8dd2c85bcd6f8956c03c7f1ff363bf16212a0733
dphyfe <davidtphyfe@gmail.com>
2025-01-16 11:26:00 -0500

    Day 16: Python basics - Classes (part 2/4)

--- day16_classes.py
+++ day16_classes.py
@@ -37,3 +37,54 @@ class MyPerson:
 my_david = MyPerson("David", "Asheville")
 my_david.my_add_hobby("hiking")
 my_david.my_add_hobby("coding")
+print(my_david.my_info())
+print(my_david.my_list_hobbies())
+
+# My class with attributes (3)
+class MyBook:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        self.read_pages = 0
+    
+    def my_read(self, num_pages):
+        self.read_pages += num_pages
+    
+    def my_progress(self):
+        percent = (self.read_pages / self.pages) * 100
+        return f"{percent:.1f}%"
+
+# Reading my book
+my_book = MyBook("Python Basics", "John Doe", 300)
+my_book.my_read(75)
+my_book.my_read(75)
+print(f"Progress: {my_book.my_progress()}")
+
+# My class methods (static) (4)
+class MyMath:
+    @staticmethod
+    def my_add(a, b):
+        return a + b
+    
+    @staticmethod
+    def my_multiply(a, b):
+        return a * b
+
+print("My static add:", MyMath.my_add(5, 3))
+print("My static multiply:", MyMath.my_multiply(4, 7))
+
+# My class variable (shared across instances) (5)
+class MyCounter:
+    my_count = 0
+    
+    def __init__(self, name):
+        self.name = name
+        MyCounter.my_count += 1
+    
+    def my_total(self):
+        return f"Total counters: {MyCounter.my_count}"
+
+my_c1 = MyCounter("first")
+
+# Progress: part 2/4
