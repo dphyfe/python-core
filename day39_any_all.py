94a1e1363f3bda540d0d220dec2f35fb7a572b55
dphyfe <davidtphyfe@gmail.com>
2025-02-08 11:28:00 -0500

    Day 39: Python advanced - Any All (part 1/4)

---
+++ day39_any_all.py
@@ -0,0 +1,21 @@
+# Day 39: any all sum min max for my practice
+
+# My any function (1)
+my_list = [False, False, True, False]
+print(f"My any: {any(my_list)}")
+
+my_all_false = [False, False, False]
+print(f"My any all false: {any(my_all_false)}")
+
+# My all function (2)
+my_all_true = [True, True, True]
+print(f"My all: {all(my_all_true)}")
+
+my_mixed = [True, True, False]
+print(f"My all mixed: {all(my_mixed)}")
+
+# My sum function (3)
+my_nums = [1, 2, 3, 4, 5]
+print(f"My sum: {sum(my_nums)}")
+
+# My sum with start (4)
