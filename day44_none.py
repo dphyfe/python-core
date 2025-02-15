11cdb6f9a88369844a14e6745c42401cac89d8ae
dphyfe <davidtphyfe@gmail.com>
2025-02-15 11:52:00 -0500

    Day 44: Python advanced - None (part 1/4)

---
+++ day44_none.py
@@ -0,0 +1,10 @@
+# Day 44: working with None for my practice
+
+my_var = None
+print(f"My var: {my_var}")
+print(f"My type: {type(my_var)}")
+
+if my_var is None:
+    print("My var is None")
+
+my_list = [1, None, 3, None, 5]
