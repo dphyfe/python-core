24adc72e066b018cd8417138e4f6f99c8048e7bc
dphyfe <davidtphyfe@gmail.com>
2025-02-15 13:02:00 -0500

    Day 44: Python advanced - None (part 2/4)

--- day44_none.py
+++ day44_none.py
@@ -8,3 +8,17 @@ if my_var is None:
     print("My var is None")
 
 my_list = [1, None, 3, None, 5]
+my_clean = [x for x in my_list if x is not None]
+print(f"My cleaned: {my_clean}")
+
+def my_maybe_return(flag):
+    if flag:
+        return "value"
+    return None
+
+my_result = my_maybe_return(False)
+print(f"My result: {my_result}")
+
+my_dict = {"a": 1, "b": None, "c": 3}
+
+# Progress: part 2/4
