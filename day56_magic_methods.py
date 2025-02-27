2fb5ff468360b32a9530cfdf686a6779d210e097
dphyfe <davidtphyfe@gmail.com>
2025-02-27 12:36:00 -0500

    Day 56: Python advanced - magic methods (part 2/2)

--- day56_magic_methods.py
+++ day56_magic_methods.py
@@ -21,3 +21,64 @@ my_v1 = MyVector(1, 2)
 my_v2 = MyVector(3, 4)
 my_v3 = my_v1 + my_v2
 print(f"My sum: {my_v3}")
+print(f"My equal: {my_v1 == my_v2}")
+
+class MyList:
+    def __init__(self, items):
+        self.items = items
+    
+    def __len__(self):
+        return len(self.items)
+    
+    def __getitem__(self, index):
+        return self.items[index]
+    
+    def __setitem__(self, index, value):
+        self.items[index] = value
+    
+    def __contains__(self, item):
+        return item in self.items
+
+my_list = MyList([1, 2, 3, 4, 5])
+print(f"My length: {len(my_list)}")
+print(f"My item: {my_list[2]}")
+print(f"My contains: {3 in my_list}")
+
+class MyRange:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.current = start
+    
+    def __iter__(self):
+        self.current = self.start
+        return self
+    
+    def __next__(self):
+        if self.current >= self.end:
+            raise StopIteration
+        self.current += 1
+        return self.current - 1
+
+for my_num in MyRange(1, 5):
+    print(f"My num: {my_num}")
+
+class MyContext:
+    def __enter__(self):
+        print("My entering")
+        return self
+    
+    def __exit__(self, *args):
+        print("My exiting")
+
+with MyContext():
+    print("My inside context")
+
+class MyCallable:
+    def __call__(self, x):
+        return x * 2
+
+my_func = MyCallable()
+print(f"My called: {my_func(5)}")
+
+# Progress: part 2/2
