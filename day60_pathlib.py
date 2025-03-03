4dcfbb1c00539023b3e37d0b382eda28c11f4a3f
dphyfe <davidtphyfe@gmail.com>
2025-03-03 12:40:00 -0500

    Day 60: Python advanced - Pathlib (part 2/4)

--- day60_pathlib.py
+++ day60_pathlib.py
@@ -16,3 +16,15 @@ my_cwd = Path.cwd()
 print(f"My cwd: {my_cwd}")
 
 my_path = Path("document.txt")
+print(f"My name: {my_path.name}")
+print(f"My stem: {my_path.stem}")
+print(f"My suffix: {my_path.suffix}")
+print(f"My parent: {my_path.parent}")
+
+my_path = Path("data/files/test.txt")
+print(f"My parts: {my_path.parts}")
+print(f"My parents: {list(my_path.parents)}")
+
+my_file = Path("test_file.txt")
+
+# Progress: part 2/4
