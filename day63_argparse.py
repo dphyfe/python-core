c245ddc20f8160b736d38a11a90eee806677a4b5
dphyfe <davidtphyfe@gmail.com>
2025-03-06 12:55:00 -0500

    Day 63: Python advanced - Argparse (part 2/4)

--- day63_argparse.py
+++ day63_argparse.py
@@ -11,3 +11,22 @@ print("My parser configured")
 
 my_parser2 = argparse.ArgumentParser()
 my_parser2.add_argument("-f", "--file", required=True, help="My input file")
+my_parser2.add_argument("-o", "--output", default="out.txt", help="My output file")
+
+print("My file parser configured")
+
+my_parser3 = argparse.ArgumentParser()
+my_parser3.add_argument("--mode", choices=["dev", "prod"], default="dev")
+my_parser3.add_argument("--count", type=int, default=1)
+
+print("My choices parser configured")
+
+my_parser4 = argparse.ArgumentParser()
+my_parser4.add_argument("files", nargs="+", help="My input files")
+
+print("My nargs parser configured")
+
+my_parser5 = argparse.ArgumentParser()
+my_subparsers = my_parser5.add_subparsers(dest="command")
+
+# Progress: part 2/4
